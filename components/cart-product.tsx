bcb473eb98e14aa8700f216d92855966c4dac64e
Amira ali <programming.tracks@gmail.com>
2025-10-21 13:08:09 +0300

    cart product

--- components/cart-product.tsx
+++ components/cart-product.tsx
@@ -67,7 +67,7 @@ export default function CartProduct({
           </p>
 
           <div className="flex flex-col gap-2 flex-1 min-w-0">
-            <h2 className="text-sm font-bold line-clamp-2 max-w-[80px] ltr:text-start rtl:text-end">
+            <h2 className="text-sm font-bold line-clamp-2 max-w-[80px] ltr:text-start rtl:text-end overflow-hidden text-ellipsis whitespace-nowrap">
               {product?.name}
             </h2>
           </div>
