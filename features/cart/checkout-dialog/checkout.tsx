433ee5dc860e70225741ecd7faeb564b36aa2cfd
Amira ali <programming.tracks@gmail.com>
2025-10-27 16:41:42 +0300

    fix removed file

--- features/cart/checkout-dialog/checkout.tsx
+++ features/cart/checkout-dialog/checkout.tsx
@@ -329,7 +329,7 @@ export default function Checkout({
   }
 
   // Show Success page if order succeeded and no redirect URL
-  if (success && (paymentMethod === "tabby" || paymentMethod === "cashondelivery")) {
+  if (saveOrderData?.success && (paymentMethod === "tabby" || paymentMethod === "cashondelivery")) {
     return (
       <PageWrapper>
         <Success data={saveOrderData} />
