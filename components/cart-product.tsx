51e74cf86781e5aca8f380f70654e217e74979a3
AmiraAli1612001 <programming.tracks@gmail.com>
2025-11-06 15:14:04 +0200

    fixed payment

--- components/cart-product.tsx
+++ components/cart-product.tsx
@@ -65,7 +65,7 @@ export default function CartProduct({
               {product?.price} <i className="icon-rial"></i>
             </p>
             <div className="scale-[0.85] sm:scale-100">
-              <div className="flex items-center gap-1 bg-gray-100 rounded-md w-fit mb-2 shadow-sm">
+              <div className="flex items-center gap-1 bg-gray-100 px-2 rounded-md w-fit mb-2 shadow-sm">
                 <div className="hidden sm:flex"><ReactStars edit={false} /> </div>
                 <div className="flex sm:hidden">
                   <ReactStars
